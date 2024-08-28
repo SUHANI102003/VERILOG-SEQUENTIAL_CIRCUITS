bf9578c5281b56fb2c7eddb8d1f04535adc6552a
SUHANI JAIN <jsuhani000710@gmail.com>
2024-08-28 22:46:03 +0530

    Add files via upload

---
+++ SYNCHRONOUS UP & DOWN COUNTER/SYN_UP_AND_DOWN_COUNTERS.srcs/sources_1/new/syn_down_counter.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.08.2024 20:54:28
+// Design Name: 
+// Module Name: syn_down_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module syn_down_counter(
+input clk,
+input reset_n,
+output [3:0] Q
+    );
+wire q0_next, q1_next;
+
+T_FF FF1 (
+        .clk(clk),
+        .reset_n(reset_n),
+        .T(1'b1),
+        .Q(Q[0])
+        );
+
+T_FF FF2 (
+        .clk(clk),
+        .reset_n(reset_n),
+        .T(~Q[0]),
+        .Q(Q[1])
+        );
+
+assign q0_next = ~Q[0] & ~Q[1];    
+T_FF FF3 (
+         .clk(clk),
+        .reset_n(reset_n),
+        .T(q0_next),
+        .Q(Q[2])
+        );
+ 
+assign q1_next =  ~Q[0] & ~Q[1] & ~Q[2];       
+T_FF FF4 (
+        .clk(clk),
+        .reset_n(reset_n),
+        .T(q1_next),
+        .Q(Q[3])
+        );
+      
+    
+endmodule
---
+++ SYNCHRONOUS UP & DOWN COUNTER/SYN_UP_AND_DOWN_COUNTERS.srcs/sources_1/new/syn_up_counter.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.08.2024 20:13:53
+// Design Name: 
+// Module Name: syn_up_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module syn_up_counter(
+input clk,
+input reset_n,
+output [3:0] Q
+    );
+wire q0_next, q1_next, q2_next, q3_next;
+
+assign q0_next = 1'b1; 
+T_FF FF1 (
+        .clk(clk),
+        .reset_n(reset_n),
+        .T(q0_next),
+        .Q(Q[0])
+        );
+
+assign q1_next =  q0_next & Q[0];     
+T_FF FF2 (
+        .clk(clk),
+        .reset_n(reset_n),
+        .T(q1_next),
+        .Q(Q[1])
+        );
+
+assign q2_next =  q1_next & Q[1];        
+T_FF FF3 (
+         .clk(clk),
+        .reset_n(reset_n),
+        .T(q2_next),
+        .Q(Q[2])
+        );
+ 
+assign q3_next =  q2_next & Q[2];       
+T_FF FF4 (
+        .clk(clk),
+        .reset_n(reset_n),
+        .T(q3_next),
+        .Q(Q[3])
+        );
+      
+endmodule
